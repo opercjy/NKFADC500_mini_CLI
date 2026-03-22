119af37064294bbca17949829c8bd66475853836
Jiyoung CHOI <158591410+opercjy@users.noreply.github.com>
2026-03-22 19:38:36 +0900

    Create LinkDef.h

---
+++ objects/include/LinkDef.h
@@ -0,0 +1,12 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class RawChannel+;
+#pragma link C++ class RawData+;
+#pragma link C++ class FadcBD+;
+#pragma link C++ class RunInfo+;
+
+#endif
